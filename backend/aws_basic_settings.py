981aaa61d1b7184e4de75817d26116800c181db9
cj-yourjourney <49462194+cj-yourjourney@users.noreply.github.com>
2024-11-01 13:57:33 -0700

    integrate AWS s3 with django and django uses S3 to host media images and staticfiles

---
+++ backend/aws_basic_settings.py
@@ -0,0 +1,7 @@
+# aws_basic_settings.py
+import environ
+env = environ.Env()
+
+# AWS Basic Settings
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
--- backend/urls.py
+++ backend/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -31,3 +33,6 @@ urlpatterns = [
     path("api/work-experiences/", include("work_experience.urls")),
     # re_path(r"^.*$", TemplateView.as_view(template_name="index.html")),
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
